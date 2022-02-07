18d96b4004c1e1c6a3d310caa57bee57fcc82a84
jayu1023 <73164366+jayu1023@users.noreply.github.com>
2022-02-07 21:39:51 +0530

    fixes issue

--- lib/ui/spalshscreen.dart
+++ lib/ui/spalshscreen.dart
@@ -23,6 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     final Directory _photoDir =
         new Directory('/storage/emulated/0/Whatsapp/media/.Statuses');
+    final Directory _second = new Directory('/storage/emulated/0/Pictures/');
     // final Directory _photoDir = new Directory('/storage/emulated/0/Download');
     // checkpermission();
     return Scaffold(
@@ -39,7 +40,12 @@ class _SplashScreenState extends State<SplashScreen> {
                 .where((element) => element.endsWith("jpg"))
                 .toList(growable: false);
 
-            print("--->.>>.${imageList}");
+            var storedimagelist = _second
+                .listSync()
+                .map((e) => e.path.split("/").last)
+                .where((item) => item.endsWith("jpg"))
+                .toList();
+            print("--->.>>.${storedimagelist}");
             return Container(
               margin: EdgeInsets.only(right: 5.0, left: 5.0),
               width: MediaQuery.of(context).size.width,
@@ -52,76 +58,87 @@ class _SplashScreenState extends State<SplashScreen> {
                     crossAxisCount: 2),
                 itemBuilder: (context, index) {
                   File file = File(imageList[index]);
-                  return Container(
-                    width: 100.0,
-                    height: 100.00,
-                    decoration: BoxDecoration(
+                  return Card(
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20.0)),
-                    child: Stack(
-                      alignment: Alignment.bottomRight,
-                      children: [
-                        Positioned.fill(
-                          child: Container(
+                    child: Container(
+                      width: 100.0,
+                      height: 100.00,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  image: DecorationImage(
+                                      image: FileImage(file),
+                                      fit: BoxFit.cover)),
+                            ),
+                          ),
+                          Container(
+                            width: 100.0,
                             decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20.0),
-                                image: DecorationImage(
-                                    image: FileImage(file), fit: BoxFit.cover)),
-                          ),
-                        ),
-                        Container(
-                          width: 100.0,
-                          decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.5),
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(30.0),
-                                  bottomRight: Radius.circular(30.0))),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SizedBox(
-                                width: 15.0,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 5.0, bottom: 5.0),
-                                child: InkWell(
-                                  child: Icon(
-                                    Icons.share,
-                                    color: Colors.white,
+                                color: Colors.black.withOpacity(0.5),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(30.0),
+                                    bottomRight: Radius.circular(20.0))),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  width: 15.0,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 5.0, bottom: 5.0),
+                                  child: InkWell(
+                                    child: Icon(
+                                      Icons.share,
+                                      color: Colors.white,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 10.0,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 5.0, bottom: 5.0),
-                                child: InkWell(
-                                  child: Icon(
-                                    Icons.download,
-                                    color: Colors.white,
-                                  ),
-                                  onTap: () {
-                                    savefileinstorage(imageList[index], file)
-                                        .then((value) {
-                                      if (value!['isSuccess'] == true) {
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(SnackBar(
-                                                content: Text(
-                                                    "Your File is saved")));
-                                      }
-                                    });
-                                  },
+                                SizedBox(
+                                  width: 10.0,
                                 ),
-                              ),
-                              SizedBox(
-                                width: 8.0,
-                              )
-                            ],
-                          ),
-                        )
-                      ],
+                                Visibility(
+                                  visible: !storedimagelist.contains(
+                                      imageList[index].split("/").last),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 5.0, bottom: 5.0),
+                                    child: InkWell(
+                                      child: Icon(
+                                        Icons.download,
+                                        color: Colors.white,
+                                      ),
+                                      onTap: () {
+                                        savefileinstorage(
+                                                imageList[index], file)
+                                            .then((value) {
+                                          if (value!['isSuccess'] == true) {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(SnackBar(
+                                                    content: Text(
+                                                        "Your File is saved")));
+                                          }
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8.0,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   );
                 },
@@ -156,9 +173,12 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<Map?> savefileinstorage(String? path, File img) async {
-    final result =
-        await ImageGallerySaver.saveFile(path!, name: path.split("/").last);
+    // path = path!.replaceAll(img.path.split("/").last, "");
+    // print(path);
+    final result = await ImageGallerySaver.saveFile(path!.split(".").last,
+        name: img.path.split("/").last);
     print(result);
+    setState(() {});
     return result;
   }
 }
