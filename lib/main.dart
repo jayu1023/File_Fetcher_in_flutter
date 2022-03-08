08b1cdf4681b3edff62f4c65d52143901ab0026e
jayu1023 <limbanijay24@gmail.com>
2022-03-08 09:35:11 +0530

    from jay

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:whatsappstatusdownloader/secondwp/picscreen.dart';
 import 'package:whatsappstatusdownloader/ui/dashboard.dart';
 import 'package:whatsappstatusdownloader/ui/spalshscreen.dart';
 import 'package:whatsappstatusdownloader/ui/videoscreen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SplashScreen(),
+    home: PicScreen(),
   ));
 }
--- lib/secondwp/picscreen.dart
+++ lib/secondwp/picscreen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -10,6 +12,10 @@ class PicScreen extends StatefulWidget {
 
 class _PicScreenState extends State<PicScreen> {
   bool? isgranted = false;
+  bool? iswpinstall = false;
+  List<dynamic>? imagelist = [];
+  Directory picdir =
+      new Directory('/storage/emulated/0/Whatsapp/media/.Statuses');
 
   @override
   void initState() {
@@ -25,8 +31,26 @@ class _PicScreenState extends State<PicScreen> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: isgranted == true
-            ? Text("permission granted")
-            : Text("Permission is not granted"),
+            ? Center(
+                child: iswpinstall != true
+                    ? Text("install wp")
+                    : imagelist!.length == 0
+                        ? Text("No Data")
+                        : GridView.builder(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                                    childAspectRatio: 0.8, crossAxisCount: 2),
+                            itemCount: imagelist!.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                width: 100.0,
+                                height: 100.0,
+                                color: Colors.grey,
+                                margin: EdgeInsets.all(8.0),
+                                child: Image.file(File(imagelist![index])),
+                              );
+                            }))
+            : Center(child: Text("Permission is not granted")),
       ),
     );
   }
@@ -34,6 +58,7 @@ class _PicScreenState extends State<PicScreen> {
   void getpermission() {
     Permission.storage.isGranted.then((value) {
       if (value == true) {
+        getdata();
         setState(() {
           isgranted = true;
         });
@@ -42,6 +67,8 @@ class _PicScreenState extends State<PicScreen> {
           setState(() {
             if (value.isGranted) {
               isgranted = true;
+              getdata();
+              setState(() {});
             } else {
               getpermission();
             }
@@ -49,5 +76,28 @@ class _PicScreenState extends State<PicScreen> {
         });
       }
     });
+  }
+
+  void getdata() {
+    try {
+      picdir.exists().then((value) async {
+        print("bjdbf");
+        print(value);
+        iswpinstall = true;
+        print("dgcytfudyfe");
+
+        imagelist = picdir
+            .listSync(recursive: true)
+            .map((item) => item.path)
+            .where((element) => element.endsWith("jpg"))
+            .toList(growable: false);
+
+        setState(() {
+          print(imagelist);
+        });
+      });
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
